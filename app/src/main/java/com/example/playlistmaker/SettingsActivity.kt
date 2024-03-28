4bbed9eef2c028f47bd3ba94826782e48c13afe9
Nikita <trenin.0235@gmail.com>
2024-03-28 10:13:30 +0700

    sprint 9 topic 4

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,24 +1,54 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
+    @SuppressLint("CutPasteId", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
         val settingsBack = findViewById<Toolbar>(R.id.settingsBack)
+        val share = findViewById<FrameLayout>(R.id.shareTheApp)
+        val writeToSupport = findViewById<FrameLayout>(R.id.writeToSupport)
+        val userAgreement = findViewById<FrameLayout>(R.id.userAgreement)
 
         settingsBack.setNavigationOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
+
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.link_to_the_practicum))
+            startActivity(Intent.createChooser(shareIntent, "Поделиться"))
+        }
+
+        writeToSupport.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.mail)))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.title_message_for_developer))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message_for_developer))
+            startActivity(shareIntent)
+        }
+
+        userAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(getString(R.string.link_to_the_agreement))
+            startActivity(intent)
+        }
+
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
