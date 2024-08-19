b786dab305e1cf3cc3a706dca334ab235c54439c
Nikita <trenin.0235@gmail.com>
2024-08-19 13:13:40 +0700

    Скрыл bottmonNavigationView в playerFragment

--- app/src/main/java/com/example/playlistmaker/ui/root/activity/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/root/activity/RootActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker.ui.root.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
@@ -21,5 +22,15 @@ class RootActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setupWithNavController(navController)
 
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.playerFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
